b21a21b14bd21bd278291cd23b5fda4c8fb40092
carlos carvajal <carlos319822@gmail.com>
2023-07-27 17:07:03 -0500

    terminando campo para subir archivo

--- src/app/components/dashboard/personas/crear-persona/crear-persona.component.ts
+++ src/app/components/dashboard/personas/crear-persona/crear-persona.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 
@@ -8,12 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './crear-persona.component.html',
   styleUrls: ['./crear-persona.component.css']
 })
-export class CrearPersonaComponent {
+export class CrearPersonaComponent implements OnInit{
 
+  public archivos : any =[]
   
   estadoc :any[]=['Solter@','Casad@','Union Libre']
   estudio: any[]=['Bachiller','Tecnico','Tecnologo','Profesional','Curso Basico']
   estado: any[]=[ 'Terminado','En Curso']
   cargo: any[]=['Asesor','Mecanico','Administrador','Oficios Varios']
   experiencia: any []=['06 Meses','1 Año','2 Años','Mas']
+
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+
+  capturarFile(event: any){
+    const archivoCapturado = event.target.files[0]
+    this.archivos.push(archivoCapturado)
+    console.log(event.target.files);
+  }
 }
