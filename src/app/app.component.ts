0080f0e76d3f5e780cda372eee7936dc4d1df5a5
carlos319822 <72896010+carlos319822@users.noreply.github.com>
2023-08-14 20:36:26 -0500

    Revert "correcciones de la tarea"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, effect, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserService } from './services/user.service';
-import { AuthStatus } from './interfaces/authenticacion';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +7,4 @@ import { AuthStatus } from './interfaces/authenticacion';
 })
 export class AppComponent {
   title = 'hmaquitec';
-
-  private loginService = inject(UserService);
-  private router = inject(Router);
-
-
-  public finishAuthenticationChecking = computed<boolean>(() => {
-    if (this.loginService.authStatus() === AuthStatus.checking) {
-      return false;
-    };
-
-    return true;
-  });
-
-
-  public authenticationStatusChangeEffect = effect(() => {
-    switch (this.loginService.authStatus()) {
-      case AuthStatus.checking:
-        break;
-
-      case AuthStatus.authenticated:
-        this.router.navigateByUrl('dashboard');
-        break;
-
-      case AuthStatus.notAuthenticated:
-        this.router.navigateByUrl('login');
-        break;
-    };
-  });
 }
