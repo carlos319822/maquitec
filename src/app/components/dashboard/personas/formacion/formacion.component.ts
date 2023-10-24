24a944ae476c0f5d6c1b5ce66c0976ee9435c8bb
Juan David <>
2023-10-24 17:32:06 -0500

    Modificación del formulario de experiencia para el soporte de múltiples formularios.

--- src/app/components/dashboard/personas/formacion/formacion.component.ts
+++ src/app/components/dashboard/personas/formacion/formacion.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-formacion',
   templateUrl: './formacion.component.html',
   styleUrls: ['./formacion.component.css']
 })
-export class FormacionComponent {
+export class FormacionComponent implements OnInit{
 
-
+  ngOnInit() {
+    this.formularioPrincipal = this.fb.group({
+      infoAcademica: this.fb.array([this.crearFormularioInfoAcademica()]),
+    });
+  }
+  
   estudio: any[] = ['Educación Basica Primaria', 'Educación Basica Secundaria', 'Bachillerato / Educacion Media', 'Universidad / Carrera Tecnica', 'Universidad / Carrera Tecnologica','Universidad / Carrera Profesional','Postgrado / Especializacion','Postgrado / Maestria','Postgrado / Doctorado', 'Otros']
 
+  formularioPrincipal: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formularioPrincipal = this.fb.group({
+      infoAcademica: this.fb.array([]),
+    });
+  }
+
+  private crearFormularioInfoAcademica() {
+    return this.fb.group({
+      educativo: [''],
+      cargo_aspirado: [''],
+      fecha_ingreso: [''],
+      fecha_retiro: ['']
+    });
+  }
+
+
+  get infoAcademica() {
+    return this.formularioPrincipal.get('infoAcademica') as FormArray;
+  }
+
+  agregarInfoAcademica() {
+    const infoForm = this.fb.group({
+      educativo: [''],
+      cargo_aspirado: [''],
+      fecha_ingreso: [''],
+      fecha_retiro: ['']
+    });
+    this.infoAcademica.push(infoForm);
+  }
+
+  eliminarInfoAcademica(index: number) {
+    this.infoAcademica.removeAt(index);
+  }
 }
